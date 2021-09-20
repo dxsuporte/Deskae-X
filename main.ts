97819231f715b7981a64555ed19ad443ee2565b9
Danilo Xavier <dxsuporteti@gmail.com>
2021-09-19 22:52:51 -0300

    adonisjs start server production

--- main.ts
+++ main.ts
@@ -4,36 +4,47 @@ import { app, Tray, Menu, nativeImage } from 'electron'
 import Path from 'path'
 //loading open browser
 import Open from 'open'
-//AdonisJS Server Start
-import './server'
+//loading child_process
+import cp from 'child_process'
+
+//AdonisJS 5 Server
+let isProd = process.env.NODE_ENV === 'production'
+async function startAdonis() {
+  cp.execFile('node', [`${Path.join(__dirname, './server.js')}`, !isProd ? '--dev' : ''])
+}
 
 let tray //creating tray variable 
 
-app.whenReady().then(() => {
-  //json conection .env
-  const conection = {
-    host: process.env.HOST,
-    port: process.env.PORT
-  }
-  //Loading tray icon image 
-  const icon = nativeImage.createFromPath(Path.join(__dirname, './', 'public', './favicon.png'))
-  //creating tray icon 
-  tray = new Tray(icon)
-  //creating icon menu 
-  var contextMenu = Menu.buildFromTemplate([
-    {
-      label: `Server ${conection.host}:${conection.port}`, click: function () {
-        Open(`http://${conection.host}:${conection.port}`); //open browser Default 
-      }
-    },
-    {
-      label: 'Stop Server', click: function () {
-        app.quit() //end application 
-      }
+(async () => {
+
+  await startAdonis() //Start AdonisJS Server
+
+  await app.whenReady().then(() => {
+    //json conection .env
+    const conection = {
+      host: process.env.HOST,
+      port: process.env.PORT
     }
-  ])
-  tray.setContextMenu(contextMenu)
-})
+    //Loading tray icon image 
+    const icon = nativeImage.createFromPath(Path.join(__dirname, './', 'public', './favicon.png'))
+    //creating tray icon 
+    tray = new Tray(icon)
+    //creating icon menu 
+    var contextMenu = Menu.buildFromTemplate([
+      {
+        label: `Server ${conection.host}:${conection.port}`, click: function () {
+          Open(`http://${conection.host}:${conection.port}`); //open browser Default 
+        }
+      },
+      {
+        label: 'Stop Server', click: function () {
+          app.quit() //end application 
+        }
+      }
+    ])
+    tray.setContextMenu(contextMenu)
+  })
+})();
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
